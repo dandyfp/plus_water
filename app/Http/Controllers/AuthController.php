1380b3402868af44eaf1f3658643ea8e38f87db7
dandyfp <dandyfp@sakutambah.id>
2023-05-27 11:58:59 +0700

    add api regis rf id

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,33 +9,33 @@ use App\Models\LoggedInRfid;
 class AuthController extends Controller
 {
     public function authenticateRF(Request $request)
-{
-    $data = $request->all();
+    {
+            $data = $request->all();
 
-    $userData = data::where('rf_id', $data['rf_id'])->first();
+            $userData = data::where('rf_id', $data['rf_id'])->first();
 
-    if ($userData) {
-        // Menyimpan data pengguna yang telah login
-        LoggedInRfid::create([
-            'nama' => $userData->nama,
-            'rf_id' => $userData->rf_id,
-            'telpon' => $userData->telpon,
-            'created_at' => $userData->waktu_login,
-            'jenis_kelamin'=>$userData->jenis_kelamin,
+            if ($userData) {
+                // Menyimpan data pengguna yang telah login
+                LoggedInRfid::create([
+                    'nama' => $userData->nama,
+                    'rf_id' => $userData->rf_id,
+                    'telpon' => $userData->telpon,
+                    'created_at' => $userData->waktu_login,
+                    'jenis_kelamin'=>$userData->jenis_kelamin,
 
-            // Tambahkan atribut lain yang ingin Anda simpan
-        ]);
+                    // Tambahkan atribut lain yang ingin Anda simpan
+                ]);
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Authenticated successfully'
-        ]);
-    } else {
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Authentication failed'
-        ]);
-    }
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'Authenticated successfully'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Authentication failed'
+                ]);
+            }
 
  /*    if ($userData) {
         return response()->json([
@@ -48,6 +48,24 @@ class AuthController extends Controller
             'message' => 'Authentication failed'
         ]);
     } */
-}
+    }
+
+    public function register(Request $request)
+        {
+            $validatedData = $request->validate([
+                'rf_id' => 'required',
+                'nama' => 'required',
+                'jenis_kelamin' => 'required',
+                'telpon' => 'required',
+
+            ]);
+
+            $user = data::create($validatedData);
+
+            return response()->json([
+                'message' => 'User registered successfully',
+                'user' => $user
+            ], 201);
+        }
 
 }
---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\data;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+}
